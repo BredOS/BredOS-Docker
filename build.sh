205a73bd6d030e638ffc74da1bbe970d23c1d580
Bill Sideris <bill88t@feline.gr>
2024-10-02 17:14:02 +0300

    Add retries to pacstrap

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ arches=(aarch64 armv7h x86_64)
 
 # Ensure we have arch-install-scripts qemu-user-static and binfmt-qemu-static installed
 sudo pacman -S --noconfirm --needed arch-install-scripts qemu-user-static qemu-user-static-binfmt
+
+# Define packages as space-separated strings
 base="base sed gzip archlinux-keyring bredos-mirrorlist bredos-keyring bredos-logo bred-os-release sudo arch-install-scripts nano base-devel"
 armv7h_packages="archlinuxarm-keyring $base"
 aarch64_packages="archlinuxarm-keyring bredos-multilib $base"
@@ -11,15 +13,31 @@ x86_64_packages="$base"
 
 script_dir=$(dirname "$(readlink -f "$0")")
 
-# build for arch lulw
+# Build for arch function
 build_arch() {
     local arch=$1
     rm -rf "$script_dir/build/rootfs" || true
     sudo mkdir -pv "$script_dir/build/rootfs"
     sudo cp -rv "$script_dir/rootfs/$arch/"* "$script_dir/build/rootfs"
     sudo cp -rv "$script_dir/rootfs/common/"* "$script_dir/build/rootfs"
+
+    # Reference the correct package variable based on the architecture
     local packages_var="${arch}_packages"
-    sudo pacstrap -c -C "$script_dir/pacman.conf.$arch" -G -M "$script_dir/build/rootfs" ${!packages_var}
+    local packages="${!packages_var}"  # Expand the string containing the packages
+
+    # Construct the pacstrap command
+    strap_cmd="sudo pacstrap -c -C \"$script_dir/pacman.conf.$arch\" -G -M \"$script_dir/build/rootfs\" $packages"
+
+    retries=0
+    max_retries=10
+    while ! eval "$strap_cmd"; do
+      retries=$((retries+1))
+      if [ "$retries" -ge "$max_retries" ]; then
+        echo "Failed to pacstrap after $retries attempts."
+        exit 1
+      fi
+      echo "Retrying... ($retries/$max_retries)"
+    done
 }
 
 # Chek for arg
